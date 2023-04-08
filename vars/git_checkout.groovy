ab998a91819aa89bd7ffb3528d371e521ccf9e3b
uday <uday>
2023-04-08 17:13:22 +0530

    changes

--- vars/git_checkout.groovy
+++ vars/git_checkout.groovy
@@ -1,6 +1,6 @@
 def git_checkout(String repo, String branch) {
     echo "This is for git checkout."
-    git credentialsid: 'Jenkins' url: "git@github.com:udaydevOpslearning/${repo}.git", branch: "${branch}"
+    git credentialsid: 'Jenkins', url: "git@github.com:udaydevOpslearning/${repo}.git", branch: "${branch}"
 }
 
 return this
